940fee38bca7de604eae201b43e3b0eea76001a7
Peter Wagner <peter.wagner@shopify.com>
2023-01-04 09:34:18 -0500

    github-v49

--- templater/workflows.go
+++ templater/workflows.go
@@ -9,7 +9,7 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
-	"github.com/google/go-github/v48/github"
+	"github.com/google/go-github/v49/github"
 	"github.com/sirupsen/logrus"
 )
 
